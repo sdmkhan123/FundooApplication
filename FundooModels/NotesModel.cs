ff5d4fad337786597724d53e326497d287bbcb37
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-01 07:31:13 +0530

    [Saddam]Add: Implemented Notes Model

---
+++ FundooModels/NotesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FundooModels
+{
+    public class NotesModel
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public virtual RegisterModel RegisterModel { get; set; }
+        public string Title { get; set; }
+        public string TakeANotes { get; set; }
+        public string RemindeMe { get; set; }
+        public string ChangeColor { get; set; }
+        public string AddImage { get; set; }
+        [DefaultValue(false)]
+        public bool PinNote { get; set; }
+        [DefaultValue(false)]
+        public bool Archive { get; set; }
+        [DefaultValue(false)]
+        public bool Trash { get; set; }
+    }
+}
