7a5b233e811840b4ff5a4fc106f60395c026a9c1
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-06 01:19:38 +0530

    [Saddam]Add: Adding migration and update database

---
+++ FundooRepository/Migrations/20211205185831_CollaboratorMigration.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class CollaboratorMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NotesModel_Users_UserId",
+                table: "NotesModel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NotesModel",
+                table: "NotesModel");
+
+            migrationBuilder.RenameTable(
+                name: "NotesModel",
+                newName: "Notes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_NotesModel_UserId",
+                table: "Notes",
+                newName: "IX_Notes_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notes",
+                table: "Notes",
+                column: "NoteId");
+
+            migrationBuilder.CreateTable(
+                name: "Collaborators",
+                columns: table => new
+                {
+                    CollaboratorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteId = table.Column<int>(nullable: false),
+                    EmailId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collaborators", x => x.CollaboratorId);
+                    table.ForeignKey(
+                        name: "FK_Collaborators_Notes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Notes",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collaborators_NoteId",
+                table: "Collaborators",
+                column: "NoteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Collaborators");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notes",
+                table: "Notes");
+
+            migrationBuilder.RenameTable(
+                name: "Notes",
+                newName: "NotesModel");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notes_UserId",
+                table: "NotesModel",
+                newName: "IX_NotesModel_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NotesModel",
+                table: "NotesModel",
+                column: "NoteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NotesModel_Users_UserId",
+                table: "NotesModel",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
