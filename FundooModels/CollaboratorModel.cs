ff07184a4b4a9198c28b11fd7fc021f00b081285
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-05 22:24:51 +0530

    [Saddam]Add: Create Collaborator model class

--- FundooModels/CollaboratorModel.cs
+++ FundooModels/CollaboratorModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace FundooModels
 {
     public class CollaboratorModel
     {
+        /// <summary>
+        /// added CollaboratorId as a primary key
+        /// </summary>
+        [Key]
+        public int CollaboratorId { get; set; }
+        public int NoteId { get; set; }
+        [ForeignKey("NoteId")]
+        public NotesModel notesModel { get; set; }
+        public string EmailId { get; set; }
     }
 }
