3429368325122e16b1d04daf559a3821f35557b0
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-06 12:52:35 +0530

    [Saddam]Add: Implementd Model class of LabelModel

--- FundooModels/LabelModel.cs
+++ FundooModels/LabelModel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace FundooModels
 {
-    class LabelModel
+    public class LabelModel
     {
+        [Key]
+        public int LabelId { get; set; }
+        public int? NoteId { get; set; }
+        [ForeignKey("NoteId")]
+        public NotesModel NotesModel { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public RegisterModel registerModel { get; set; }
+        [Required]
+        public string LabelName { get; set; }
     }
 }
