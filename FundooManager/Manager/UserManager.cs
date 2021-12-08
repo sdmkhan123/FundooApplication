51b5dde0c7493741c7b6fd61f0e97bedded25bf8
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-08 22:49:28 +0530

    [Saddam]Add: Adding StyleCop

--- FundooManager/Manager/UserManager.cs
+++ FundooManager/Manager/UserManager.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="UserController.cs" company="Magic Soft">
+// <copyright file="UserManager.cs" company="Magic Soft">
 //   Copyright © 2021 Company="Magic Soft"
 // </copyright>
 // <creator name="Saddam Khan"/>
@@ -13,16 +13,30 @@ namespace FundooManager.Interface
     using FundooModels;
     using FundooRepository.Interface;
 
+    /// <summary>
+    /// Class user manager
+    /// </summary>
     public class UserManager : IUserManager
     {
-        //Declaring obj for the IUserRepository
+        /// <summary>
+        /// Declaring object for the IUserRepository
+        /// </summary>
         private readonly IUserRepository repository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserManager"/> class
+        /// </summary>
+        /// <param name="repository">taking repository as parameter</param>
         public UserManager(IUserRepository repository)
         {
             this.repository = repository;
         }
 
+        /// <summary>
+        /// Register method for manager class
+        /// </summary>
+        /// <param name="registerModel">passing register model</param>
+        /// <returns>Returns string if Registration is successful</returns>
         public string Register(RegisterModel registerModel)
         {
             try
@@ -35,6 +49,11 @@ namespace FundooManager.Interface
             }
         }
 
+        /// <summary>
+        /// Login method finds user in database and permit him to login
+        /// </summary>
+        /// <param name="loginModel">LoginModel loginDetails</param>
+        /// <returns>returns string if login is successful or Unsuccessful</returns>
         public string LogIn(LoginModel loginModel)
         {
             try
@@ -47,6 +66,11 @@ namespace FundooManager.Interface
             }
         }
 
+        /// <summary>
+        /// Generating a JWT token
+        /// </summary>
+        /// <param name="email">passing email as a parameter of type string</param>
+        /// <returns>Returns a string of token</returns>
         public string JwtToken(string email)
         {
             try
@@ -59,6 +83,11 @@ namespace FundooManager.Interface
             }
         }
 
+        /// <summary>
+        /// Method for Resetting new Password
+        /// </summary>
+        /// <param name="resetPasswordModel">Passing resetPasswordModel of type ResetPasswordModel</param>
+        /// <returns>Returns string if the password is successfully reset</returns>
         public string ResetPassword(ResetPasswordModel resetPasswordModel)
         {
             try
@@ -71,6 +100,11 @@ namespace FundooManager.Interface
             }
         }
 
+        /// <summary>
+        /// Forgot password method performs sending mail to user,for creating new password
+        /// </summary>
+        /// <param name="emailId">string email</param>
+        /// <returns>Returns a string value as mail sent successfully</returns>
         public string ForgotPassword(string emailId)
         {
             try
