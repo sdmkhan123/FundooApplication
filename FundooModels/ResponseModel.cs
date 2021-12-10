fd2f908576f5b5ca12df3c723608ed8793a67602
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-10 16:15:48 +0530

    [Saddam]Add: Adding StyleCop

--- FundooModels/ResponseModel.cs
+++ FundooModels/ResponseModel.cs
@@ -1,20 +1,35 @@
 ﻿// ************************************************************************************
-// <copyright file="ResetPasswordModel.cs" company="Magic Soft">
+// <copyright file="ResponseModel.cs" company="Magic Soft">
 //   Copyright © 2021 Company="Magic Soft"
 // </copyright>
 // <creator name="Saddam Khan"/>
 // ************************************************************************************
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FundooModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Response model class
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
     public class ResponseModel<T>
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether status as true or false
+        /// </summary>
         public bool Status { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the message as string
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data as generic type
+        /// </summary>
+        public T Data { get; set; }
     }
 }
